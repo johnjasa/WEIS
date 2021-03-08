28e29b3d15a0e9cdcba52ff7dca7daafeb084933
John Jasa <johnjasa11@gmail.com>
2021-03-07 23:22:52 -0700

    Commenting out tip defl

--- WISDEM/wisdem/glue_code/gc_PoseOptimization.py
+++ WISDEM/wisdem/glue_code/gc_PoseOptimization.py
@@ -797,7 +797,7 @@ class PoseOptimization(object):
             wt_opt["rs.constr.max_strainU_spar"] = blade_constr["strains_spar_cap_ss"]["max"]
             wt_opt["rs.constr.max_strainL_spar"] = blade_constr["strains_spar_cap_ps"]["max"]
             wt_opt["stall_check.stall_margin"] = blade_constr["stall"]["margin"] * 180.0 / np.pi
-            wt_opt["tcons.max_allowable_td_ratio"] = blade_constr["tip_deflection"]["margin"]
+            # wt_opt["tcons.max_allowable_td_ratio"] = blade_constr["tip_deflection"]["margin"]
 
         if self.modeling["flags"]["nacelle"] and self.modeling["WISDEM"]["DriveSE"]["direct"]:
             drive_constr = self.opt["constraints"]["drivetrain"]
